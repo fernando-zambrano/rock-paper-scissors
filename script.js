0919bd7cf0c6bb5ccdffe60611dfca4f1c4d8118
fernando-zambrano <zambranoefs@gmail.com>
2022-07-28 21:57:01 +0100

    Finish the game by adding game and restartGame functions

--- script.js
+++ script.js
@@ -1,20 +1,13 @@
-// Randomly generate computer's choice
-// Prompt the user requesting their choice
-// Make sure user's input is case insensitive
-// Compare user's vs computer's choice and determine the winner
-// Keep playing for 5 rounds
-// Declare the winner
-// Ask user whether they want to restart the game
-// Restart the game or end it, based on user's choice
-
 const options = ["Rock", "Paper", "Scissors"];
 
 function getComputerChoice() {
+  // Randomly generate computer's choice
   let choicePosition = Math.floor(Math.random() * 3);
   return options[choicePosition];
 }
 
 function getPlayerChoice() {
+  // Prompt the user requesting their choice
   let playerInput;
   let invalidInput = true;
 
@@ -25,6 +18,7 @@ function getPlayerChoice() {
       return false;
     }
 
+    // Make sure user's input is case insensitive
     playerSelection = playerInput[0].toUpperCase() + playerInput.substring(1).toLowerCase();
 
     if (!(options.includes(playerSelection))) {
@@ -33,34 +27,112 @@ function getPlayerChoice() {
       invalidInput = false;
     }
 
-  } while (invalidInput)
+  } while (invalidInput);
   
   return playerSelection; 
 }
 
 function playRound(playerSelection, computerSelection) {
+  // Compare user's vs computer's choice and determine the winner
   
   if ((playerSelection === "Paper" && computerSelection === "Rock") || 
   (playerSelection === "Rock" && computerSelection === "Scissors") || 
   (playerSelection === "Scissors" && computerSelection === "Paper")) {
-    return (`You win! ${playerSelection} beats ${computerSelection}.`)
+    alert(`You win! ${playerSelection} beats ${computerSelection}.`)
+    return "player";
   
   } else if ((computerSelection === "Paper" && playerSelection === "Rock") ||
   (computerSelection === "Rock" && playerSelection === "Scissors") || 
   (computerSelection === "Scissors" && playerSelection === "Paper")) {
-    return (`You lose! ${computerSelection} beats ${playerSelection}.`)
+    alert(`You lose! ${computerSelection} beats ${playerSelection}.`)
+    return "computer";
 
-  } else {
-    return (`It's a tie! Both selected ${computerSelection}.`); 
+  } else if (!playerSelection) {
+    return "No input";
   }
+    else {
+    alert(`It's a tie! Both selected ${computerSelection}.`); 
+  }
+  
 }
 
-let playerChoice = getPlayerChoice();
-let computerChoice = getComputerChoice();
+function game() {
+  // Keep playing for 5 rounds
 
-if (!playerChoice) {
-  alert("You didn't make any choice. Game over.")
-} else {
-  console.log(playRound(playerChoice, computerChoice));
+  let playerChoice;
+  let computerChoice ;
+  let playerScore = 0;
+  let computerScore = 0;
+  let result;
+  let keepGoing = true;
+
+  for (let i = 0; i < 5; i++) {
+
+    playerChoice = getPlayerChoice();
+    computerChoice = getComputerChoice();
+
+    result = playRound(playerChoice, computerChoice);
+
+    if (result === "player") {
+      playerScore += 1;
+    } else if (result === "computer") {
+      computerScore += 1;
+    } else if (result === "No input") {
+      alert("You didn't make any choice and broke the game.\n\nGame over.");
+      keepGoing = false;
+      break
+    } else {
+      i -= 1;
+    }
+
+    alert(`Current score\nPlayer: ${playerScore}\nComputer: ${computerScore}`);
+
+  } 
+
+  if(keepGoing) {
+    // Declare the winner
+    if (playerScore > computerScore) {
+      alert("You won the game! :D");
+    } else {
+      alert("You lost the game :(")
+    } 
+  }
+
 }
+
+function restartGame() {
+  // Restart the game or end it, based on user's choice
+
+  let playAgainOptions = ["y", "n"];
+  let playAgainInput;
+  let invalidInput = true;
+
+  do { 
+    let playAgain = prompt("Do you want to play again? Y/N");
+
+    if(!playAgain) {
+      alert("You didn't make any choice and broke the game.\n\nGame over.");
+      break
+    } else {
+      playAgainInput = playAgain.toLowerCase();
+    }
+
+    if(!playAgainOptions.includes(playAgainInput)) {
+      alert(`${playAgainInput} is not a valid input -.-`);
+    } else {
+      invalidInput = false;
+    }
+
+    if(playAgainInput === "y") {
+      game();
+      invalidInput = true;
+    } else {
+      alert("Goodbye!");
+    }
+
+  } while(invalidInput);
+}
+
+game();
+restartGame();
 
